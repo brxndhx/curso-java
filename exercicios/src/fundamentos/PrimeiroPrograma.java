75b6ae06106eaf31a13760924cacd6a7987b0dab
Brendha Vitória <69943493+brxndhx@users.noreply.github.com>
2021-09-09 14:52:11 -0300

    Update PrimeiroPrograma.java

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,27 @@
 package fundamentos;
 
+/**
+ * 
+ * @author bvitoria
+ *
+ */
+
 public class PrimeiroPrograma {
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		
+		//Comentário de uma linha
+		
 		System.out.println("Primeiro programa!!!");
+		
+		/*Comentário de
+		 * várias
+		 * linhas
+		 */
+		System.out.println("Primeiro programa #2!!!");
 	}
 
 }
