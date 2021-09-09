fa93b08f14cba2cbea431340d60a40755ff95d83
Brendha Vitória <69943493+brxndhx@users.noreply.github.com>
2021-09-09 17:29:52 -0300

    Add files via upload

---
+++ exercicios/src/fundamentos/Temperatura.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class Temperatura {
+	public static void main(String[] args) {
+		
+		double fare = 86;
+		final int x = 32;
+		final double y = 5/9.0;
+		double cel = (fare - x) * y;
+		
+		
+		System.out.println(fare + "°F quando convertidos são " + cel + "em °C"); 
+		
+		
+	}
+
+}
